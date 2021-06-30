6ce67c307c48e5f2ebdf6b1dcbc3cd463911726a
Ismail17stats <55184325+Ismail17stats@users.noreply.github.com>
2021-06-30 09:46:42 +0200

    Data cleaning and mutating some variables

---
+++ General Social Survey US _ Multiple Linear Regression.R
@@ -0,0 +1,31 @@
+getwd()
+library(readxl)
+library(tidyverse)
+library(Stat2Data)
+library(skimr)
+GSS_data <- read_excel("GSS Clean Final Version Ismail.xlsx")
+
+
+GSS_data <- GSS_data %>% 
+  mutate(age_numeric = parse_number(age_of_respondent))
+GSS_data$highest_year_school_completed_father <- as.numeric(GSS_data$highest_year_school_completed_father)
+#high_scool_father <- GSS_data$highest_year_school_completed_father 
+
+#high_scool_father <- gsub(",", "", high_scool_father)
+
+#high_scool_father <- as.numeric(high_scool_father)
+
+
+ggplot(data = GSS_data) + 
+
+geom_point(aes(y = highest_year_of_school_completed, 
+               x = highest_year_school_completed_father ), alpha =0.06)
+
+
+#geom_point(aes(y = jitter(highest_year_of_school_completed), 
+#               x = jitter(high_scool_father)))
+
+
+model1 <- lm(highest_year_of_school_completed ~ highest_year_school_completed_father , data = GSS_data)
+
+coef(model1)
